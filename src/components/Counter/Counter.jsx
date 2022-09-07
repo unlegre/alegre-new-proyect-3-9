9c2d8dcfb035d60776e52be2dd0cfedbbae695f4
“gabriel <unlegre@gmail.com>
2022-09-07 00:37:52 -0300

    se agrego contador funcional sin iconos

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,30 +1,36 @@
 import React, { useState } from 'react'
 import './Counter.css'
-import {IoRemoveCircleOutline} from 'react-icons/Io';
-import {GrAddCircle} from  'react-icons/Gr';
+// import {IoRemoveCircleOutline} from 'react-icons/Io';
+// import {GrAddCircle} from  'react-icons/Gr';
 // importo button
-import ResetButton from "../ResetButton/ResetButton.jsx";
+//import ResetButton from "./ResetButton/ResetButton.jsx";
 
 
 const Counter = () => {
   const [count,setCount]=useState(1);
 
   const increment =()=>{
+
     setCount(count+1);
   }
   const decrement=()=>{
-    setCount(count-1);
+    if(count > 1){
+      setCount(count-1);
+    }
+  
   }
-  const reset=()=>{
+ /* const reset=()=>{
     setCount(1);
-  }
+  }*/
 
   return (
   <div> 
     <div className='Counter'>
-      
-    </div> */
-    <ResetButton reset={reset}/>
+      < button onClick={decrement} > - </button>
+      <p className='Counter-number'>{count}</p>
+      <button onClick={increment} > + </button>
+    </div> 
+ 
   </div>  
   );
 }
